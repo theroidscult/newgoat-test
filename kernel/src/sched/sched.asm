47b4fc59029145acf78d23684460c0e3117f85f1
lolguy91 <retek1544@gmail.com>
2024-12-21 12:08:16 +0100

    added proper contexts

--- kernel/src/sched/sched.asm
+++ kernel/src/sched/sched.asm
@@ -2,43 +2,46 @@ global timer_isr
 extern sys_timer_isr
 
 %macro pushaq 0
-    push rax
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    push rbp
-    push rsp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rbp
+	push rdi
+	push rsi
+	push rdx
+	push rcx
+	push rbx
+	push rax
 %endmacro
 
 %macro popaq 0
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsp
-    pop rbp
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
     pop rax
+    pop rbx
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rbp
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
 %endmacro
 
 timer_isr:
+    push 0
+
     pushaq
+    mov rdi, rsp
     call sys_timer_isr
     popaq
     iretq
--- kernel/src/sched/sched.c
+++ kernel/src/sched/sched.c
@@ -1,7 +1,8 @@
 #include <sys/pic.h>
 #include <dev/cereal.h>
+#include <sys/context.h>
 
-void sys_timer_isr() {
+void sys_timer_isr(context_t* context) {
     cereal_write(0x3F8, 'a');
     pic_eoi(0);
 }
