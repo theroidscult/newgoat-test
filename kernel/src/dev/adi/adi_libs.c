1da798e092e8205f1534bef26d2280b8da9669c0
lolguy91 <retek1544@gmail.com>
2025-02-08 12:26:37 +0100

    [BROKEN] added metalanguage implementation creation code

--- kernel/src/dev/adi/adi_libs.c
+++ kernel/src/dev/adi/adi_libs.c
@@ -3,7 +3,9 @@
 #include <printf.h>
 #include <stdint.h>
 #include <string.h>
+#include <logger.h>
 
+#include <mm/mm.h>
 
 metalang_x86_64_t x86_64_m = (metalang_x86_64_t){
     
@@ -30,8 +32,36 @@ metalang_fb_t fb_m = (metalang_fb_t){
 };
 
 metalanguage_t storage_init(__attribute__((unused)) event_identify identify,__attribute__((unused)) event_transact transact){
-    kprintf("[STUB] storage_init\n");
-    return (metalanguage_t){.id = 0, .name = "storage", .lang_impl = 0, .params = NULL};
+    log_trace("storage_init\n");
+
+    if(identify == NULL || transact == NULL) {
+        log_trace("Storage metalang implementation failed to register\n");
+        return (metalanguage_t){.id = 0, .name = NULL, .lang_impl = 0, .params = NULL};
+    }
+
+    object_t fptrs = (object_t){
+        .type = OBJ_TYPE_GENERIC_FPTRS,
+        .data.generic_fptrs = {
+            .fptrs[0] = (uintptr_t)identify,
+            .fptrs[1] = (uintptr_t)transact
+        }
+    };
+
+    uint32_t fptrs_id = mm_store_obj(&fptrs);
+
+    object_t timekeeper = (object_t){
+        .type = OBJ_TYPE_METALANG_IMPL,
+        .data.metalang_impl = {
+            .id    = 0x400F0000, //TODO: make it a define
+            .fptrs = fptrs_id
+        }
+    };
+
+    uint32_t metalang_id = mm_store_obj(&timekeeper);
+
+    log_trace("Storage metalang implementation registered");
+
+    return (metalanguage_t){.id = 0x400F0000, .name = "misc_storage", .lang_impl = (uintptr_t)metalang_id, .params = NULL};
 }
 
 bool storage_signal_transaction_done(__attribute__((unused)) adi_device_t* dev,__attribute__((unused)) uint32_t id){
@@ -45,15 +75,43 @@ metalang_storage_t storage_m = (metalang_storage_t){
 };
 
 metalanguage_t timekeeper_init(__attribute__((unused)) event_get_time get_time,__attribute__((unused)) event_set_time set_time,__attribute__((unused)) event_get_time_unix get_time_unix,__attribute__((unused)) event_set_time_unix set_time_unix){
-    kprintf("[STUB] timekeeper_init\n");
-    return (metalanguage_t){.id = 0, .name = "timekeeper", .lang_impl = 0, .params = NULL};
+    log_trace("timekeeper_init\n");
+
+    if(get_time == NULL || set_time == NULL || get_time_unix == NULL || set_time_unix == NULL) {
+        log_debug("Timekeeper initialization failed\n");
+        return (metalanguage_t){.id = 0, .name = NULL, .lang_impl = 0, .params = NULL};
+    }
+
+    object_t fptrs = (object_t){
+        .type = OBJ_TYPE_GENERIC_FPTRS,
+        .data.generic_fptrs = {
+            .fptrs[0] = (uintptr_t)get_time,
+            .fptrs[1] = (uintptr_t)set_time,
+            .fptrs[2] = (uintptr_t)get_time_unix,
+            .fptrs[3] = (uintptr_t)set_time_unix
+        }
+    };
+
+    uint32_t fptrs_id = mm_store_obj(&fptrs);
+
+    object_t timekeeper = (object_t){
+        .type = OBJ_TYPE_METALANG_IMPL,
+        .data.metalang_impl = {
+            .id    = 0x400F0001, //TODO: make it a define
+            .fptrs = fptrs_id
+        }
+    };
+
+    uint32_t metalang_id = mm_store_obj(&timekeeper);
+
+    return (metalanguage_t){.id = 0x400F0001, .name = "misc_timekeeper", .lang_impl = (uintptr_t)metalang_id, .params = NULL};
 }
 
 metalang_timekeeper_t timekeeper_m = (metalang_timekeeper_t){
     .init = timekeeper_init
 };
 
-void log_info(char* format,...) {
+void _log_info(char* format,...) {
     char buffer[1024];
     ksnprintf(buffer, 1024,"[LOG] %s", format);
     va_list args;
@@ -61,7 +119,7 @@ void log_info(char* format,...) {
     kvprintf(buffer,args);
     va_end(args);
 }
-void log_warning(char* format,...) { 
+void _log_warning(char* format,...) { 
     char buffer[1024];
     ksnprintf(buffer, 1024,"[WARN] %s", format);
     va_list args;
@@ -69,7 +127,7 @@ void log_warning(char* format,...) {
     kvprintf(buffer,args);
     va_end(args);
 }
-void log_error(char* format,...) { 
+void _log_error(char* format,...) { 
     char buffer[1024];
     ksnprintf(buffer, 1024,"[ERROR] %s", format);
     va_list args;
@@ -78,7 +136,7 @@ void log_error(char* format,...) {
     va_end(args);
 }
 
-void panic(char* format,...) {
+void _panic(char* format,...) {
     char buffer[1024];
     ksnprintf(buffer, 1024,"[PANIC] %s", format);
     va_list args;
@@ -134,10 +192,10 @@ adi_core_t core = (adi_core_t){
     .misc_storage = &storage_m,
     .misc_timekeeper = &timekeeper_m,
 
-    .log_info = log_info,
-    .log_warning = log_warning,
-    .log_error = log_error,
-    .panic = panic,
+    .log_info = _log_info,
+    .log_warning = _log_warning,
+    .log_error = _log_error,
+    .panic = _panic,
     .exit = exit,
 
     .register_device = register_device,
